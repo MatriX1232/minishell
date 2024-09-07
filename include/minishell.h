12e2983fcbfcd3443350301e4472c0381f6248dc
MatriX1232 <msolinsk@LAPTOP-1DVV0C81>
2024-09-07 20:18:04 +0200

    Created Makefile

---
+++ include/minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 19:13:09 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/09/07 19:16:14 by msolinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "../libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdbool.h>
+# include <dirent.h>
+
+#endif
