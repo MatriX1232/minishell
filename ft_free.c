7ef076dafd9d503d0d9ea5cc6e93bfde0da57ef9
Code_MatriX <85500588+MatriX1232@users.noreply.github.com>
2024-10-16 23:06:22 +0200

    Major fixes | Added exit codes of builtins

---
+++ ft_free.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_free.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/07 19:49:30 by msolinsk          #+#    #+#             */
+/*   Updated: 2024/10/16 13:33:31 by msolinsk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+//	Free the shell structure
+void	ft_free_shell(t_minishell *shell)
+{
+	if (shell->cwd)
+		free(shell->cwd);
+	if (shell->print_prefix_success)
+		free(shell->print_prefix_success);
+	if (shell->print_prefix_failure)
+		free(shell->print_prefix_failure);
+	if (shell->parms == NULL)
+		free(shell->parms);
+	if (shell->env)
+		ft_free_env(shell->env);
+	if (shell->raw_parsed)
+		free(shell->raw_parsed);
+	free(shell);
+}
+
+//	Free the parameters in form of a char**
+void	ft_free_parms(t_minishell *shell)
+{
+	int	i;
+
+	if (shell->quotes == true)
+	{
+		free(shell->parms[0]);
+		free(shell->parms[1]);
+	}
+	else
+	{
+		i = 0;
+		while (shell->parms[i])
+		{
+			free(shell->parms[i]);
+			i++;
+		}
+		free(shell->parms);
+	}
+	shell->parms = NULL;
+}
