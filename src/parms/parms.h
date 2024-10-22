4c38779907403e2f1fdd5983f522f148ad9355c1
MatriX1232 <msolinsk@LAPTOP-1DVV0C81>
2024-10-22 17:51:57 +0200

    Added ft_add_parm function

--- src/parms/parms.h
+++ src/parms/parms.h
@@ -6,15 +6,18 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 13:39:27 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/10/22 13:39:49 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:52:32 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARM_H
-# define PARM_H
+#ifndef PARMS_H
+# define PARMS_H
 
 # include "../../include/minishell.h"
 # include "../../include/structures.h"
 # include "../../include/libraries.h"
+
+void	ft_rm_parm(t_minishell *shell, int idx);
+void	ft_add_parm(t_minishell *shell, int idx, char *value);
 
 #endif
