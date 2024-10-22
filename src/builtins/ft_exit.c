767dfadcee393b38348737a6939e590e4cc7d1bc
MatriX1232 <msolinsk@LAPTOP-1DVV0C81>
2024-10-22 13:13:21 +0200

    Added argc to echo | Added fixed norminette in ft_exit.c

--- src/builtins/ft_exit.c
+++ src/builtins/ft_exit.c
@@ -6,14 +6,14 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 22:24:42 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/10/16 23:22:13 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/10/22 12:37:19 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../quotes/quotes.h"
 
-static int free_exit(t_minishell *shell, char *line, char *msg, int exit_code)
+static int	free_exit(t_minishell *shell, char *line, char *msg, int exit_code)
 {
 	ft_error(shell, msg, 0);
 	ft_free_shell(shell);
@@ -23,7 +23,9 @@ static int free_exit(t_minishell *shell, char *line, char *msg, int exit_code)
 
 int	ft_exit(t_minishell *shell, char *line)
 {
-	int	i;
+	int		i;
+	bool	f_1;
+	bool	f_2;
 
 	if (!shell->parms[1])
 		exit(free_exit(shell, line, NULL, 0));
@@ -32,7 +34,13 @@ int	ft_exit(t_minishell *shell, char *line)
 	i = 0;
 	while (shell->parms[1][i])
 	{
-		if (!ft_isdigit(shell->parms[1][i]) && !ft_intab(shell->parms[1][i], " +-"))
+		f_1 = false;
+		f_2 = false;
+		if (!ft_isdigit(shell->parms[1][i]))
+			f_1 = true;
+		if (!ft_intab(shell->parms[1][i], " +-"))
+			f_2 = true;
+		if (f_1 && f_2)
 			exit(free_exit(shell, line, "numeric argument required\n", 2));
 		i++;
 	}
