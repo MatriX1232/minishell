6c3cee84da284b9c67d2cae4dcbecc33df79f2ad
msolinsk <msolinsk@student.42warsaw.pl>
2024-10-24 11:42:40 +0200

    Fixed cd command

--- src/builtins/cd/ft_cd.c
+++ src/builtins/cd/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 14:12:54 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/10/16 22:45:04 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/10/24 11:02:52 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,24 @@
 int	ft_cd(t_minishell *shell)
 {
 	int		i;
+	char	*dir;
 	char	*msg;
 
 	i = ft_tablen(shell->parms, 0);
 	if (i != 2)
+		dir = ft_strdup(ft_get_var_value(shell, "HOME"));
+	else
+		dir = ft_strdup(shell->parms[1]);
+	if (chdir(dir) == -1)
 	{
 		ft_add_var(shell, ft_strdup("?=1"), 1);
-		msg = ft_strdup("Usage: cd <directory>\n");
-		ft_error(shell, msg, 0);
-		free(msg);
-		return (EXIT_FAILURE);
-	}
-	if (chdir(shell->parms[1]) == -1)
-	{
-		ft_add_var(shell, ft_strdup("?=1"), 1);
-		msg = ft_strjoin("Could not change directory: ", shell->parms[1]);
+		msg = ft_strjoin("Could not change directory: ", dir);
 		msg = ft_strjoin_free(msg, "\n", 1, 0);
 		ft_error(shell, msg, 0);
 		free(msg);
+		free(dir);
 		return (EXIT_FAILURE);
 	}
+	free(dir);
 	return (EXIT_SUCCESS);
 }
