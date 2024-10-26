ef3c79c736a096000306f668a75629e17071b057
msolinsk <msolinsk@student.42warsaw.pl>
2024-10-27 00:19:00 +0200

    Removed 'fixes' for ignoring unclosed quotes

--- src/ft_quote_parser.c
+++ src/ft_quote_parser.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:41:31 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/10/23 19:10:05 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/10/27 00:18:04 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,6 @@ void	ft_squote(char *str, char *ret, int *i)
 	j = *i + 1;
 	while (str[j] && str[j] != 39)
 		j++;
-	if (!str[j])
-		j--;
 	new = ft_substr(str, *i + 1, j - *i - 1);
 	ft_strlcat(ret, new, 10000);
 	free(new);
@@ -39,13 +37,7 @@ void	ft_dquote(t_minishell *shell, char *str, char *ret, int *i)
 	j = *i + 1;
 	while (str[j] && str[j] != '\"')
 		j++;
-	if (!str[j] || j == *i + 1)
-	{
-		*i = j;
-		return ;
-	}
-	else
-		tmp = ft_pdquote(shell, str + *i);
+	tmp = ft_pdquote(shell, str + *i);
 	ft_strlcat(ret, tmp, 10000);
 	free(tmp);
 	(*i)++;
@@ -105,3 +97,12 @@ void	ft_qparser_shell(t_minishell *shell, char *str)
 	ft_move_split(shell, ret);
 	free(ret);
 }
+
+	// int j = 0;
+	// while (shell->parms[j])
+	// {
+	// 	printf("shell.parms[%d]: %s\n", j, shell->parms[j]);
+	// 	j++;
+	// }
+	// if (shell->parms[j] == NULL)
+	// 	printf("shell.parms[%d]: (null)\n", j);
