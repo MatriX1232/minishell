eaae2e15f3ed22c9f871af3a3f41518696e76a1f
msolinsk <msolinsk@student.42warsaw.pl>
2024-10-27 14:43:56 +0100

    Added STDIN_FILENO and isatty to main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: msolinsk <msolinsk@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 19:12:42 by msolinsk          #+#    #+#             */
-/*   Updated: 2024/10/26 22:32:46 by msolinsk         ###   ########.fr       */
+/*   Updated: 2024/10/27 14:40:49 by msolinsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,70 +15,29 @@
 #include "quotes/quotes.h"
 #include "builtins/builtins.h"
 
-/*
-	Allocate memory for the shell structure
-
-	Return SUCCESS:
-		Pointer to the allocated memory
-
-	Return FAILURE:
-		NULL
-*/
-t_minishell	*ft_malloc_shell(t_minishell *shell)
-{
-	shell = (t_minishell *) malloc(1 * sizeof(t_minishell));
-	if (!shell)
-	{
-		ft_error(NULL, "Could not allocate memory for shell\n", 1);
-		return (NULL);
-	}
-	return (shell);
-}
-
-static void	ft_tshell_init(t_minishell *shell, char *argv[], char **envp)
-{
-	shell->print_prefix_success = NULL;
-	shell->print_prefix_failure = NULL;
-	shell->cwd = NULL;
-	shell->print_prefix_success = ft_strdup("\x1b[33m^_^  minishell ▶ \x1b[0m");
-	if (!shell->print_prefix_success)
-		return (ft_error(shell, E_MALLOC, 1), exit(EXIT_FAILURE));
-	shell->print_prefix_failure = ft_strdup("＞︿＜ minishell ▶ ");
-	if (!shell->print_prefix_failure)
-		return (ft_error(shell, E_MALLOC, 1), exit(EXIT_FAILURE));
-	shell->cwd = (char *) malloc(2048 * sizeof(char));
-	if (!shell->cwd)
-		return (ft_error(shell, E_MALLOC, 1), exit(EXIT_FAILURE));
-	shell->parms = NULL;
-	shell->argv = argv;
-	shell->env = ft_init_env(envp);
-}
-
-static int	ft_init(t_minishell **shell, int ac, char **argv, char **envp)
-{
-	(void)ac;
-	printf("\n");
-	outline("MINISHELL", "Welcome to our MINISHELL project!", \
-	RED, YELLOW);
-	outline("AUTHORS", "idomagal | msolinsk", GREEN, GREEN);
-	*shell = ft_malloc_shell(*shell);
-	if (*shell == NULL)
-		return (EXIT_FAILURE);
-	ft_tshell_init(*shell, argv, envp);
-	signal(SIGINT, sig_ctrlc);
-	signal(SIGQUIT, SIG_IGN);
-	signal(SIGTERM, sig_term);
-	signal(SIGSEGV, sig_segv);
-	signal(SIGABRT, sig_abort);
-	return (EXIT_SUCCESS);
-}
-
 static int	ft_on_exit(t_minishell *shell, char *line)
 {
 	printf("exit\n");
 	free(line);
 	ft_free_shell(shell);
+	rl_clear_history();
 	return (EXIT_SUCCESS);
+}
+
+static char	*ft_readline(t_minishell *shell)
+{
+	char	*line;
+
+	if (isatty(STDIN_FILENO))
+		line = readline(shell->print_prefix_success);
+	else
+	{
+		printf("%s", shell->print_prefix_success);
+		line = get_next_line(STDIN_FILENO);
+	}
+	if (line)
+		add_history(line);
+	return (line);
 }
 
 int	main(int argc, char *argv[], char **envp)
@@ -93,11 +52,9 @@ int	main(int argc, char *argv[], char **envp)
 	while (line)
 	{
 		free(line);
-		line = readline(shell->print_prefix_success);
+		line = ft_readline(shell);
 		if (line == NULL)
 			return (ft_free_shell(shell), printf("exit\n"), EXIT_SUCCESS);
-		if (line)
-			add_history(line);
 		if (ft_strncmp(line, "exit", 5) == 0)
 			break ;
 		if (ft_check_line(line) == 1)
